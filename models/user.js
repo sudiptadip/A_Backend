efab5affede62c17ada9accadbddb9494871cce0
sudiptadip <sudiptabhattacharjee000@gmail.com>
2023-04-10 02:29:43 +0530

    add data

--- models/user.js
+++ models/user.js
@@ -3,16 +3,19 @@ const mongoosh = require("mongoose");
 const userSchema = new mongoosh.Schema({
   name: { require: true, type: String },
   email: { require: true, type: String },
-  bio: {type: String},
-  url: {type: String},
+  bio: { type: String },
+  url: { type: String },
+  avtarUrl: { type: String },
+  type: { type: String },
+  template: { type: String },
   created_at: {
     type: Date,
     default: new Date(),
   },
   updated_at: {
     type: Date,
-    default: ''
-  }
+    default: "",
+  },
 });
 
 const newusers = mongoosh.model("user", userSchema);
