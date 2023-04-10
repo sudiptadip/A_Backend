4185cb40760e83300b0f79a16e3ec128e6d3a973
sudiptadip <sudiptabhattacharjee000@gmail.com>
2023-04-10 19:29:01 +0530

    add data

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
-const CONNECTION_URL = "mongodb+srv://sudip123:sudip123@cluster0.mgfuqey.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://xyz1234:xyz1234@cluster101.zluhluy.mongodb.net/?retryWrites=true&w=majority"
 const connection = mongoose.connect(CONNECTION_URL);
 
 module.exports = {connection}
