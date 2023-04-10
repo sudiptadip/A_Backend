d56fe67c8cba687b94e48b3fb3f41eb297b94710
sudiptadip <sudiptabhattacharjee000@gmail.com>
2023-04-10 09:19:56 +0530

    change database

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
-const CONNECTION_URL = "mongodb+srv://xyz123:xyz123@cluster0.8j16wlh.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://sudip123:sudip123@cluster0.mgfuqey.mongodb.net/?retryWrites=true&w=majority"
 const connection = mongoose.connect(CONNECTION_URL);
 
 module.exports = {connection}
